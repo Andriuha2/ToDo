c4c661fe6a8902ce476a906ae7768f5be97ea0f2
Andriejus <andriejusgataveckas@gmail.com>
2022-09-04 20:26:49 +0300

    pridieti helpers

---
+++ helpers/dateHelpers.js
@@ -0,0 +1,12 @@
+export const formatDate = (date) => {
+  const addZero = (num) => (num < 10 ? "0" + num : num);
+
+  const h = addZero(new Date(date).getHours());
+  const min = addZero(new Date(date).getMinutes());
+  const s = addZero(new Date(date).getSeconds());
+  const d = addZero(new Date(date).getDate());
+  const mon = addZero(new Date(date).getMonth() + 1);
+  const y = new Date(date).getFullYear();
+
+  return `${h}:${min}:${s} \xa0 ${d}-${mon}-${y}`;
+};
--- index.js
+++ index.js
@@ -1,52 +1,18 @@
+import { formatDate } from "./helpers/dateHelpers.js";
+import { addTask } from "./helpers/taskHelpers.js";
+
 let sortByName = false;
 let sortByDate = false;
+let taskStatus = "active"; //status can be: active and completed
 
-//function to draw a new task
-const addTask = () => {
-  const inputValue = document.getElementById("taskInput").value;
-  const input = document.getElementById("taskInput");
-
-  if (!inputValue.trim()) {
-    alert("Incorrect value. Check if provided");
-    input.value = null;
-    return;
-  }
-
-  const newTask = {
-    id: new Date().getTime(),
-    task: inputValue,
-  };
-
-  const lsArr = JSON.parse(window.localStorage.getItem("tasks"));
-  let arr = lsArr ? lsArr : [];
-  arr.push(newTask);
-
-  window.localStorage.setItem("tasks", JSON.stringify(arr));
-
-  input.value = null;
-  drawTaskList();
-};
-
-//function to read date
-const formatDate = (date) => {
-  const addZero = (num) => (num < 10 ? "0" + num : num);
-
-  const h = addZero(new Date(date).getHours());
-  const min = addZero(new Date(date).getMinutes());
-  const s = addZero(new Date(date).getSeconds());
-  const d = addZero(new Date(date).getDate());
-  const mon = addZero(new Date(date).getMonth() + 1);
-  const y = new Date(date).getFullYear();
-
-  return `${h}:${min}:${s} \xa0 ${d}-${mon}-${y}`;
-};
-
-document.getElementById("addBtn").addEventListener("click", addTask);
+document.getElementById("addBtn").addEventListener("click", () => {
+  addTask(drawTaskList);
+});
 
 //draw new task on Enter
 document.getElementById("taskInput").addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    addTask();
+    addTask(drawTaskList);
   }
 });
 
@@ -66,27 +32,38 @@ const drawTaskList = () => {
     arr.sort((a, b) => (a.id > b.id ? 1 : b.id > a.id ? -1 : 0));
   }
 
+  // filtering active
+  if (taskStatus === "active") {
+    arr = arr.filter(({ status }) => status === "active");
+  }
+
+  // filtering completed
+  if (taskStatus === "completed") {
+    arr = arr.filter(({ status }) => status === "completed");
+  }
+
   arr.forEach((singleTask, ind) => {
     //creating elements
     const myLi = document.createElement("li");
     const myInput = document.createElement("input");
     const myLabel = document.createElement("label");
-    // date group
     const myDate = document.createElement("div");
     //btn group
     const btnGroup = document.createElement("div");
     const deleteBtn = document.createElement("button");
     const editBtn = document.createElement("button");
+    const completeBtn = document.createElement("button");
 
     //adding styles
     myLi.className = "row d-flex justify-content-between list-group-item";
     myInput.className = "form-check-input me-1 col-1";
-    myLabel.className = "form-check-label col-5";
-    myDate.className = "col-4";
+    myLabel.className = "form-check-label col-4";
+    myDate.className = "col-5";
 
     btnGroup.className = "btn-group col-2";
     deleteBtn.className = "btn btn-outline-danger btn-sm ";
     editBtn.className = "btn btn-outline-primary btn-sm";
+    completeBtn.className = "btn btn-outline-success btn-sm";
 
     //adding other attributes
     //input
@@ -111,16 +88,34 @@ const drawTaskList = () => {
     editBtn.setAttribute("type", "button");
     editBtn.textContent = "Edit";
 
+    // complete on click
+    completeBtn.setAttribute("type", "button");
+    completeBtn.textContent = "Done";
+
     //append childs
     //handle btn group append
-    btnGroup.append(editBtn, deleteBtn);
+    switch (taskStatus) {
+      case "active":
+        btnGroup.append(editBtn, completeBtn, deleteBtn);
+        break;
+      case "completed":
+        btnGroup.append(deleteBtn);
+        break;
+      default:
+        btnGroup.append(editBtn, completeBtn, deleteBtn);
+        break;
+    }
+
     myLi.append(myInput, myLabel, myDate, btnGroup);
     tasksList.append(myLi);
 
     //task events
     deleteBtn.addEventListener("click", () => {
-      arr = arr.filter((filterTask) => filterTask.id !== singleTask.id);
-      window.localStorage.setItem("tasks", JSON.stringify(arr));
+      console.log(lsArr);
+      const filteredArr = lsArr.filter(
+        (filterTask) => filterTask.id !== singleTask.id
+      );
+      window.localStorage.setItem("tasks", JSON.stringify(filteredArr));
       drawTaskList();
     });
 
@@ -133,29 +128,106 @@ const drawTaskList = () => {
           task: updatedTask,
         };
 
-        arr.splice(ind, 1, newTask);
+        const updatedArr = lsArr.map((task) =>
+          task.id === singleTask.id ? newTask : task
+        );
+
+        window.localStorage.setItem("tasks", JSON.stringify(updatedArr));
+        drawTaskList();
+      }
+    });
+
+    completeBtn.addEventListener("click", () => {
+      if ((singleTask.status = "active")) {
+        const newTask = {
+          ...singleTask,
+          status: "completed",
+        };
+
+        const updatedArr = lsArr.map((task) =>
+          task.id === singleTask.id ? newTask : task
+        );
+
+        window.localStorage.setItem("tasks", JSON.stringify(updatedArr));
         drawTaskList();
       }
     });
   });
 };
 
+// sorting by name
 document.getElementById("sortByName").addEventListener("click", function () {
   sortByName = !sortByName;
-  sortByDate = false;
-  document.getElementById("sortByDate").classList.remove("active");
-
   sortByName ? this.classList.add("active") : this.classList.remove("active");
+  document.getElementById("sortByDate").classList.remove("active");
+  sortByDate = false;
   drawTaskList();
 });
 
+// sorting by date
 document.getElementById("sortByDate").addEventListener("click", function () {
   sortByDate = !sortByDate;
-  sortByName = false;
-  document.getElementById("sortByName").classList.remove("active");
-
   sortByDate ? this.classList.add("active") : this.classList.remove("active");
+  document.getElementById("sortByName").classList.remove("active");
+  sortByName = false;
+  drawTaskList();
+});
+
+// filtering active
+document.getElementById("activeTask").addEventListener("click", function () {
+  taskStatus = "active";
+  document.getElementById("completedTask").classList.remove("active-nav");
+  this.classList.add("active-nav");
+  drawTaskList();
+});
+
+// filtering completed
+document.getElementById("completedTask").addEventListener("click", function () {
+  taskStatus = "completed";
+  document.getElementById("activeTask").classList.remove("active-nav");
+  this.classList.add("active-nav");
   drawTaskList();
 });
 
 drawTaskList();
+
+function getWeather() {
+  fetch("https://weatherdbi.herokuapp.com/data/weather/alanija")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      const weatherContainer = document.getElementById("weather");
+
+      //create elements
+      const weatherInfo = document.createElement("div");
+      const weatherImg = document.createElement("img");
+      const weatherDescription = document.createElement("h5");
+      const weatherTemp = document.createElement("h3");
+      const weatherText = document.createElement("div");
+      const location = document.createElement("h1");
+      const date = document.createElement("h3");
+
+      //adding styles
+      weatherInfo.className = "weather-info";
+      weatherTemp.className = "temperature";
+      weatherText.className = "weather-text";
+
+      //adding attributes
+      weatherImg.setAttribute("src", data.currentConditions.iconURL);
+      weatherImg.setAttribute("alt", "weather icon");
+
+      //adding textContent
+      weatherDescription.textContent = data.currentConditions.comment;
+      weatherTemp.textContent = `°C ${data.currentConditions.temp.c}`;
+      location.textContent = data.region;
+      date.textContent = data.currentConditions.dayhour;
+
+      //appending
+      weatherInfo.append(weatherImg, weatherDescription);
+      weatherText.append(location, date);
+
+      weatherContainer.append(weatherInfo, weatherTemp, weatherText);
+    });
+}
+getWeather();
